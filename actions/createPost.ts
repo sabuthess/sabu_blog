423fbb4ddb393dbb378dae352a5f171503abb6e8
sabuthess <dilan.developer22@gmail.com>
2025-11-07 02:00:46 -0500

    add imageUrl to prisma.post.create

--- actions/createPost.ts
+++ actions/createPost.ts
@@ -1,28 +1,36 @@
 "use server";
 
-import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
-import { revalidatePath } from "next/cache";
-
-import { redirect } from "next/navigation";
+import prisma from "@/lib/prisma";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
+export async function createPost(data: {
+	title: string;
+	content: string;
+	imageUrl?: string;
+}) {
+	const session = await getServerSession(authOptions);
+	if (!session) throw new Error("No autorizado");
 
+	const { title, content, imageUrl } = data;
+	try {
+		const post = await prisma.post.create({
+			data: {
+				title,
+				content,
+				imageUrl: imageUrl || null,
+				authorId: session?.user?.id,
+			},
+		});
 
-export const createPost = async (formData: FormData) => {
-	const title = formData.get("title") as string;
-	const content = formData.get("content") as string;
-	const session = await getServerSession(authOptions)
-
-	await prisma.post.create({
-		data: {
-			title,
-			content,
-			authorId: session.user.id,
-		},
-	});
+		console.log("Post creado:", post);
+	} catch (error) {
+		console.error("Error creating post:", error);
+		throw new Error("Error al crear el post");
+	}
 
 	revalidatePath("/");
 	redirect("/");
-	
-};
+}
