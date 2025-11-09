3b18e20404d7fbb6059d3be9a386768958918175
sabuthess <dilan.developer22@gmail.com>
2025-11-08 22:55:19 -0500

    Add  DashboardPage by comments

---
+++ app/dashboard/comments/page.tsx
@@ -0,0 +1,13 @@
+import { DashboardSkeleton } from "@/app/components/DashboardSkeleton";
+
+function DashboardCommentsPage() {
+    // const { data: session } = useSession();
+    
+
+    return (
+        <DashboardSkeleton>
+            <h1>My posts comments here</h1>
+        </DashboardSkeleton>
+    );
+}
+export default DashboardCommentsPage;
