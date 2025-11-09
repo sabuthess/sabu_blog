efbc1b9bf5944022559294761782a3a99b46b930
sabuthess <dilan.developer22@gmail.com>
2025-11-08 22:56:20 -0500

    Add  DashboardPage by favorites

---
+++ app/dashboard/favorites/page.tsx
@@ -0,0 +1,13 @@
+import { DashboardSkeleton } from "@/app/components/DashboardSkeleton";
+
+function DashboardFavoritesPage() {
+	// const { data: session } = useSession();
+	
+
+	return (
+		<DashboardSkeleton>
+            <h1>My favorites posts here</h1>
+		</DashboardSkeleton>
+	);
+}
+export default DashboardFavoritesPage;
